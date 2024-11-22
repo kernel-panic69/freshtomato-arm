0ce92cb30c3de66a9d2b4158536572f066627245
pedro <pedro311@gmail.com>
2024-11-22 17:32:13 +0100

    Improve UPnP IGD & PCP/NAT-PMP
    - Improve wording and GUI
    - Remove 'Show In My Network Places' option as icon is always shown in Windows
    - Remove 'Inactive Rules Cleaning' options as not working
    - Use secure mode setting for PCP
    - Improve listing and rearrange table headers
    - Invert/reword secure mode
    
    Thanks @Self-Hosting-Group

--- release/src-rt-6.x.4708/router/shared/defaults.c
+++ release/src-rt-6.x.4708/router/shared/defaults.c
@@ -85,15 +85,11 @@ struct nvram_tuple upnp_defaults[] = {
 	{ "upnp_secure",		"1"				, 0 },
 	{ "upnp_port",			"0"				, 0 },
 	{ "upnp_ssdp_interval",		"900"				, 0 },	/* SSDP interval */
-	{ "upnp_mnp",			"0"				, 0 },
 	{ "upnp_custom",		""				, 0 },
 	{ "upnp_lan",			""				, 0 },
 	{ "upnp_lan1",			""				, 0 },
 	{ "upnp_lan2",			""				, 0 },
 	{ "upnp_lan3",			""				, 0 },
-	{ "upnp_clean",			"1"				, 0 },	/* 0:Disable 1:Enable */
-	{ "upnp_clean_interval",	"600"				, 0 },	/* Cleaning interval in seconds */
-	{ "upnp_clean_threshold",	"20"				, 0 },	/* Threshold for cleaning unused rules */
 #if 0	/* disabled for miniupnpd */
 	{ "upnp_max_age",		"180"				, 0 },	/* Max age */
 	{ "upnp_config",		"0"				, 0 },
